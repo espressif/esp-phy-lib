c0491ee7cc60288244268b04b523637a6e297739
zhangwenxu <zhangwenxu@espressif.com>
2022-03-16 19:43:47 +0800

    support libphy&libbtbb for esp32h2beta2
    * add esp32h2 CI test
    * update fix_printf.sh
    * rename h2beta1 libphy&libbtbb
    * add libphy&libbtbb for esp32h2beta2
      libbtbb version: 6c47ec3
      libphy version: bb2a234

--- fix_printf.sh
+++ fix_printf.sh
@@ -22,6 +22,21 @@ for dir in esp32 esp32s2 esp32c3 esp32s3 esp32h2; do
                 echo $dir/librtc.a fixed
                 $TOOLCHAIN-objcopy --redefine-sym ets_printf=rtc_printf librtc.a
             fi
+        elif [ $dir = esp32h2 ]; then
+            for subdir in rev1 rev2 ; do
+                cd $subdir
+                git status libbtbb.a | grep "modified" >/dev/null 2>&1
+                if [ $? -eq 0 ]; then
+                    echo $dir/libbtbb.a fixed
+                    $TOOLCHAIN-objcopy --redefine-sym ets_printf=rtc_printf libbtbb.a
+                fi
+                git status libphy.a | grep "modified" >/dev/null 2>&1
+                if [ $? -eq 0 ]; then
+                    echo $dir/libphy.a fixed
+                    $TOOLCHAIN-objcopy --redefine-sym ets_printf=phy_printf libphy.a
+                fi
+                cd ..
+            done
         elif [ $dir != esp32s2 ]; then
             git status libbtbb.a | grep "modified" >/dev/null 2>&1
             if [ $? -eq 0 ]; then
@@ -29,11 +44,12 @@ for dir in esp32 esp32s2 esp32c3 esp32s3 esp32h2; do
                 $TOOLCHAIN-objcopy --redefine-sym ets_printf=rtc_printf libbtbb.a
             fi
         fi
-
-        git status libphy.a | grep "modified" >/dev/null 2>&1
-        if [ $? -eq 0 ]; then
-            echo $dir/libphy.a fixed
-            $TOOLCHAIN-objcopy --redefine-sym ets_printf=phy_printf libphy.a
+        if [ $dir != esp32h2 ]; then
+            git status libphy.a | grep "modified" >/dev/null 2>&1
+            if [ $? -eq 0 ]; then
+                echo $dir/libphy.a fixed
+                $TOOLCHAIN-objcopy --redefine-sym ets_printf=phy_printf libphy.a
+            fi
         fi
 
         cd ..
