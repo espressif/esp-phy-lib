f1d9b9b5cb63dac81b9027f50f7a46b1d840ce5c
lixiaofan <lixiaofan@espressif.com>
2023-09-26 11:49:28 +0800

    add librftest.a

--- fix_printf.sh
+++ fix_printf.sh
@@ -45,11 +45,13 @@ for dir in esp32 esp32s2 esp32c3 esp32s3 esp32h2 esp32h4 esp32c2 esp32c6; do
             echo $dir/libphy.a fixed
             $TOOLCHAIN-objcopy --redefine-sym ets_printf=phy_printf libphy.a
         fi
-        if [ $dir = esp32c3 ] || [ $dir = esp32s3 ]; then
-            git status libbttestmode.a | grep "modified\|new file" >/dev/null 2>&1
-            if [ $? -eq 0 ]; then
-                echo $dir/libbttestmode.a fixed
-                $TOOLCHAIN-objcopy --redefine-sym ets_printf=phy_printf libbttestmode.a
+        if [ $dir = esp32c3 ] || [ $dir = esp32s3 ] || [ $dir = esp32c2 ] || [ $dir = esp32c6 ] || [ $dir = esp32s2 ]; then
+            if [ $dir != esp32s2 ]; then
+            	git status libbttestmode.a | grep "modified\|new file" >/dev/null 2>&1
+            	if [ $? -eq 0 ]; then
+                	echo $dir/libbttestmode.a fixed
+                	$TOOLCHAIN-objcopy --redefine-sym ets_printf=phy_printf libbttestmode.a
+            	fi
             fi
             git status librfate.a | grep "modified\|new file" >/dev/null 2>&1
             if [ $? -eq 0 ]; then
